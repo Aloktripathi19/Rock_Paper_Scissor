02fa7ba70c13788088ef27b4c829ddf7e68ad5c3
Aloktripathi19 <tripathialok1919@gmail.com>
2024-11-05 01:10:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let my = 0, cy = 0
+        function fun(ch) {
+            let game = ['ROCK', 'PAPER', 'SCISSOR']
+            g = game[(Math.floor(Math.random() * game.length))]
+
+            document.getElementById("min1").innerHTML = ch
+            document.getElementById("min2").innerHTML = g
+
+            if (ch == 'ROCK' && g == 'SCISSOR' || ch == 'PAPER' && g == 'ROCK' || ch == 'SCISSOR' && g == 'PAPER') {
+                document.getElementById("min3").innerHTML = 'YOU WON'
+                document.getElementById("min3").style.color = 'green'
+                my++
+                document.getElementById("min4").innerHTML = my
+
+            }
+            else if (ch === g) {
+                document.getElementById("min3").innerHTML = 'DRAW'
+                document.getElementById("min3").style.color = 'blue'
+            }
+            else {
+                document.getElementById("min3").innerHTML = 'YOU LOOSE'
+                document.getElementById("min3").style.color = "red"
+                cy++
+                document.getElementById("min5").innerHTML = cy
+            }
+        }
